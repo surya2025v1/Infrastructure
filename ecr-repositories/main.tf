8adf4d014de508ca96e2b08448651bd381dbb2c1
surya2025v1 <surya2025v1@gmail.com>
2025-10-02 16:51:36 -0400

    Update main.tf

--- ecr-repositories/main.tf
+++ ecr-repositories/main.tf
@@ -16,14 +16,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Data source for existing public ECR repository
-data "aws_ecr_repository" "public_clients_code" {
-  name = "clients-code"
-}
-
-# Output the public ECR repository information for Lambda usage
+# Public ECR repository information (no data source needed)
 locals {
-  public_registry_url = "public.ecr.aws/x7o9n0b1/clients-code"
+  public_registry_url      = "public.ecr.aws/x7o9n0b1/clients-code"
+  repository_name          = "clients-code"
+  repository_url           = "public.ecr.aws/x7o9n0b1/clients-code"
+  registry_id             = "x7o9n0b1"  # Your public ECR registry ID
+  registry_region         = "us-east-1"
 }
 
 output "public_ecr_repository_url" {
@@ -34,10 +33,10 @@ output "public_ecr_repository_url" {
 output "public_ecr_repository_metadata" {
   description = "Public ECR repository metadata"
   value = {
-    name         = data.aws_ecr_repository.public_clients_code.name
-    repository_uri = data.aws_ecr_repository.public_clients_code.repository_url
-    registry_id  = data.aws_ecr_repository.public_clients_code.registry_id
-    arn          = data.aws_ecr_repository.public_clients_code.arn
+    name           = local.repository_name
+    repository_url = local.repository_url
+    registry_id    = local.registry_id
+    region         = local.registry_region
   }
 }
 
@@ -46,7 +45,7 @@ output "usage_instructions" {
   description = "Instructions for using the public ECR repository"
   value = {
     docker_pull_command = "docker pull ${local.public_registry_url}:latest"
-    lambda_image_uri = "${data.aws_ecr_repository.public_clients_code.repository_url}:latest"
+    lambda_image_uri = "${local.repository_url}:latest"
     public_read_access = "This repository is publicly accessible and can be used by Lambda functions without additional permissions"
     authentication_required = false
   }
